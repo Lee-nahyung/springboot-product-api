c15ea32796dcef12a7e5fe87419f2d62d4f8ee4d
Lee-nahyung <skgud0125@naver.com>
2025-09-04 16:29:10 +0900

    주석 추가

--- src/main/java/com/example/productapi/common/config/SecurityConfig.java
+++ src/main/java/com/example/productapi/common/config/SecurityConfig.java
@@ -27,9 +27,9 @@ public class SecurityConfig {
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         // API 엔드포인트
-                        .requestMatchers("/api/login", "/api/token/refresh", "/api/logout").permitAll()
+                        .requestMatchers("/api/login", "/api/token/refresh", "/api/logout").permitAll() //이 api에 대해서는 인증이 없이 된다는 것.
                         // Swagger UI v3 (springdoc-openapi)
-                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                        .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll() //다른 api에 대해서는 토큰없이 보면 403 뜸.
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
